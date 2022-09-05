68586c62c05699e43686bcbccd0143beea18a981
buentellopc <buentellopc@com>
2022-09-05 11:55:10 -0500

    More component work and styling with css classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hey there</div>;
+  return (
+    <div className="App">
+      <h1>My todos</h1>
+      <div className="card">
+        <h2>TITLE</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
