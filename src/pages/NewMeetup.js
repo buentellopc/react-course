6bedeee6715ac1d4ecaef6f21c30c5666d515d93
buentellopc <buentellopc@gmail.com>
2022-09-06 12:57:06 -0500

    Preparing the app for http

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,11 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 export default function NewMeetupPage(props) {
+  function addMeetupHandler(meetupData) {}
   return (
     <section>
       <h1>Add Newmeetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
