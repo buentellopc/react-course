e0170441a8127dd183cd60c1294a9a2c0c2dc3d5
buentellopc <buentellopc@com>
2022-09-07 23:01:54 -0500

    More context usage

--- src/pages/Favorite.js
+++ src/pages/Favorite.js
@@ -5,10 +5,18 @@ import FavoritesContext from "../store/favorites-context";
 export default function FavoritePage(props) {
   const favoritesCtx = useContext(FavoritesContext);
 
+  let content;
+
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. Start adding some?</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+
   return (
     <div>
       <h1>My Favorites</h1>
-      <MeetupList meetups={favoritesCtx.favorites} />
+      {content}
     </div>
   );
 }
